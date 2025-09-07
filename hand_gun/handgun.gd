6fb5189f695c08be1c521f067d2b416200789b02
Marcondes-Amarante <117780345+Marcondes-Amarante@users.noreply.github.com>
2025-09-07 12:36:15 -0300

    feat: movimentacao personagem e sistema de tiro

---
+++ hand_gun/handgun.gd
@@ -0,0 +1,75 @@
+extends Node2D
+
+@onready var gun: AnimatedSprite2D = $spriteHandGun
+@onready var gunMarkerLeft: Marker2D = $gunPositionLeft
+@onready var posCano: Marker2D = $cano
+@onready var posCanoLeft: Marker2D = $canoLeft
+
+const BULLET = preload("res://bullet/bullet.tscn")
+
+var municao_atual: int = 6
+var maximo_municao: int = 6
+var recarregar: bool = false
+
+func _ready() -> void:
+	#conectando sinal de mudança de frame da animação de gun a função de gerar bala
+	gun.frame_changed.connect(_criar_bala)
+	gun.animation_finished.connect(_recarregar_arma)
+
+func _process(delta: float):
+	
+	#ajusta rotação do nó raiz para rotacionar sempre em relação ao mouse
+	look_at(get_global_mouse_position())
+	
+	#limita rotação para ficar entre 0° e 360°
+	rotation_degrees = wrap(rotation_degrees, 0, 360)
+
+	#invertendo verticalmente sprite da arma caso rotação esteja entre 90 e 270
+	if rotation_degrees > 90 and rotation_degrees < 270:
+		gun.flip_v = true
+	else:
+		gun.flip_v = false
+		
+	#mudando gun position para o marker esquerdo caso arma tenha sido invertida em y
+	if gun.flip_v == true:
+		gun.position = gunMarkerLeft.position
+	else:
+		gun.position = Vector2(40, 0)
+		
+	#lógica do tiro
+	if Input.is_action_just_pressed("shoot") and recarregar == false:
+		
+		if municao_atual == 0:
+			recarregar = true
+			gun.animation = "reload"
+			gun.play()
+		else:
+			#acionando animacao de tiro
+			gun.animation = "gun_shoot"
+			gun.play()
+		
+func _criar_bala():
+	
+	if gun.animation == "gun_shoot" and gun.frame == 5 and recarregar == false:
+		
+		#criando instância da bala
+		var bullet_instance = BULLET.instantiate()
+		
+		#adicionando instância criado como nó filho da arma
+		get_tree().root.add_child(bullet_instance)
+		
+		#definindo posição de origem da bala como a frente do cano
+		bullet_instance.global_position = posCano.global_position
+		if gun.flip_v == true:
+			bullet_instance.global_position = posCanoLeft.global_position
+			
+		#definindo rotação da bala como rotação da arma
+		bullet_instance.rotation = rotation
+		
+		municao_atual = municao_atual-1
+		print(municao_atual)
+	
+func _recarregar_arma():
+	if gun.animation == "reload":
+		municao_atual = maximo_municao
+		recarregar = false
---
+++ main_character/player.gd
@@ -0,0 +1,68 @@
+extends Node2D
+
+var walk_speed = 100
+var run_speed = 200
+
+# executa instruções quando o nó raiz é adicionado a cena
+func _ready():
+	
+	#definindo nó raiz para o centro da tela
+	self.position = get_viewport_rect().size / 2
+	$body.position = Vector2.ZERO
+	$AreaDano.position = Vector2.ZERO
+	
+#obtendo referência dos sprites
+@onready var sprite: AnimatedSprite2D = $body
+
+#executa instruções a cada frame
+#delta = tempo desde último frame
+func _process(delta: float):
+	
+	#inicializando vetor2D (x, y) de direcao com zeros
+	#esse vetor vai armazenar uma direção (horizontal e vertical) com base em qual botão mapeado foi apertado
+	var vetor_direcao_input = Vector2.ZERO
+	
+	#captando pressionamento de teclas
+	#direita assume sempre valores positivos, enquanto esquerda negativos
+	vetor_direcao_input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	vetor_direcao_input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	#verificando se alguma tecla foi pressionada com base no valor de comprimento do vetor
+	if vetor_direcao_input.length() > 0:
+		
+		#normalizando vetor para garantir que personagem se mova na mesma proporção de comprimento em todas as direções
+		vetor_direcao_input = vetor_direcao_input.normalized()
+		
+	var speed = walk_speed
+	
+	#trocando velocidades caso shift seja apertado
+	#se tanto a direção do mouse como a global são a mesma então muda-se a velocidade
+	if (Input.is_action_pressed("ui_shift") and Input.get_action_strength("ui_left") and get_global_mouse_position().x < global_position.x) or (Input.is_action_pressed("ui_shift") and Input.get_action_strength("ui_right") and get_global_mouse_position().x > global_position.x):
+		speed = run_speed
+	
+	#usando delta para garantir que independente da quantidade de frames por segundo o deslocamento (comprimento do vetor) seja sempre o mesmo
+	position += vetor_direcao_input * speed * delta
+
+	#geranciando sprites
+	if vetor_direcao_input != Vector2.ZERO:
+		if speed == walk_speed:
+			sprite.animation = "Walk"
+		#tratando caso do personagem corre para trás:
+		#(mouse a direita + direção esquerda + shift)
+		elif (speed == run_speed and Input.get_action_strength("ui_left") and get_global_mouse_position().x > global_position.x):
+			sprite.animation = "Walk"
+		#(mouse a esquerda + direcao direita + shift)
+		elif (speed == run_speed and Input.get_action_strength("ui_right") and get_global_mouse_position().x < global_position.x):
+			sprite.animation = "Walk"
+		else:
+			sprite.animation = "Run"
+		sprite.play()
+	else:
+		sprite.animation = "Idle"
+		sprite.play()
+	
+	#invertendo sprite horizontalmente com base na posição do mouse
+	if get_global_mouse_position().x < global_position.x:
+		sprite.flip_h = true
+	else:
+		sprite.flip_h = false
